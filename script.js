f05c12a644409b3b30497c89b1ed464dee0661aa
Thailane <144239652+ThailaJJ@users.noreply.github.com>
2025-05-30 11:27:39 -0300

    sav

--- script.js
+++ script.js
@@ -1,1 +1,3 @@
 const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-principal");
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
