8ba9dfcf50896b3da6e8029d30058f7180623150
Thailane <144239652+ThailaJJ@users.noreply.github.com>
2025-05-30 11:31:28 -0300

    sava

--- script.js
+++ script.js
@@ -3,3 +3,5 @@ const caixaPerguntas = document.querySelector(".caixa-principal");
 const caixaAlternativas = document.querySelector(".caixa-alternativas");
 const caixaResultado = document.querySelector(".caixa-reultado");
 const textoresultado = document.querySelector(".texto-resultado");
+
+const perguntas = ["pergunta1", "pergunta2"]
