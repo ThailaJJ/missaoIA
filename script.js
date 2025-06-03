5fed0a03c7252d5e3e9346d1ee2629e2ae983554
Thailane <144239652+ThailaJJ@users.noreply.github.com>
2025-06-03 13:10:08 -0300

    formatado

--- script.js
+++ script.js
@@ -4,18 +4,20 @@ const caixaAlternativas = document.querySelector(".caixa-alternativas");
 const caixaResultado = document.querySelector(".caixa-reultado");
 const textoresultado = document.querySelector(".texto-resultado");
 
-const perguntas = 
-alternativas [
-{enunciado: "Pergunta1",
-alternativas: 
-["Alternativa1",
-"Alternativa2" ] 
-},
+const perguntas =
+  alternativas[
+  {
+    enunciado: "Pergunta1",
+    alternativas:
+      ["Alternativa1",
+        "Alternativa2"]
+  },
 
-{enunciado: "Pergunta1",
-  Alternativas: [
-    "Alternativas 1",
-    "Alternativa 2"
-  ]
+  {
+    enunciado: "Pergunta1",
+    Alternativas: [
+      "Alternativas 1",
+      "Alternativa 2"
+    ]
 
-} ]
+  }]
