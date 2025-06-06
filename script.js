d25fe6d5aa5a410906e83704568b04fa8225745b
Thailane <144239652+ThailaJJ@users.noreply.github.com>
2025-06-06 11:14:47 -0300

    linkando

--- script.js
+++ script.js
@@ -53,3 +53,13 @@ const perguntas =
 
   }
 ]
+
+let atual - 0;
+let perguntaAtual;
+
+function mostraPergunta() {
+  perguntaAtual = perguntas [atual]
+  caixaPerguntas.textContent = perguntaAtual.enunciado;
+}
+
+mostraPergunta ();
